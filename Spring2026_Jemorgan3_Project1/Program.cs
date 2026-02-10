26d26bf7dca606ad938384bb4313d30c6f62abc0
Jemorgan3 <Jemorgan3@crimson.ua.edu>
2026-02-10 14:15:45 -0600

    commit

--- Spring2026_Jemorgan3_Project1/Program.cs
+++ Spring2026_Jemorgan3_Project1/Program.cs
@@ -20,12 +20,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
 
 
 app.Run();
